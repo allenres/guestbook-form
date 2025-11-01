71fb744c9654446ef5ed98cf26868c637662b255
allenres <allen.resulidze@gmail.com>
2025-11-01 16:27:56 -0700

    add guestbook routing

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 
 // Enable static file serving (client side file that does not communicate with database)
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
+const records = [];
 
 // define the port number where our server will listen
 const PORT = 3002;
@@ -15,8 +18,19 @@ const PORT = 3002;
 // res: allows us to send back a response to the client
 app.get('/', (req, res) => {
     // send "Hello, World!" as a response to the client
-    res.sendFile(`${import.meta.dirname}/views/home.html`); // <- cannot have multiple of these
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
+
+app.get('/admin', (req, res) => {
+    res.send({records})
+})
+
+app.post('/submit-form', (req, res) => {
+    const record = req.body;
+    records.push(record);
+    console.log(records);
+    res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+})
 
 // start the server and make it listen on the port specified above
 app.listen(PORT, () => {
